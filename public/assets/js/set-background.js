0ea09ac530821b5db698f4eec51877f0a6d69d82
Bilal Elezi <belezi@intelycare.com>
2018-12-10 17:09:52 -0500

    chng

--- public/assets/js/set-background.js
+++ public/assets/js/set-background.js
@@ -2,5 +2,10 @@ var idx = Math.floor((new Date().getHours()));
 var body = document.getElementsByTagName("body")[0];
 body.className = "heaven-" + idx;
 
-
+var message = document.getElementsByClassName("text")[0];
+console.log(message)
+message.prepend("Hey there: ");
+console.log(message)
+message.prepend("Hey there1: ");
+message.prepend("Hey there2: ");
 console.log("in the js export.get function")
